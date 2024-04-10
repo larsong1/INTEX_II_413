470d03079555aebd7056a7294415bf62c2a1a455
HammondCameron55 <110644424+HammondCameron55@users.noreply.github.com>
2024-04-09 19:38:50 -0600

    This is where I (Cameron) will do my changes.
    I have started by bringing in the model necessary to predict whether or not a charge is fraudulent.
    
    I have made some changes and notes in the Home Controller denoting where I am going with my chagnes
    
    my priority right now is building the functionality to predict whether or not a transaction is fraudulent or not.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,21 +4,74 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
+//For Implementing the pipeline
+using Microsoft.ML;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+
 namespace INTEX_II_413.Controllers
 {
     public class HomeController : Controller
     {
         private IIntexRepository _repo;
 
+        //This is the pipeline that will be used to make predictions
+        private readonly InferenceSession _sessionFraud;
+
         public HomeController(IIntexRepository temp)
         {
             _repo = temp;
+
+            _sessionFraud = new InferenceSession("C:/Users/Hammo/source/repos/INTEX_II_413/Fraud_Identification_model.onnx");
+            
         }
+
+
 
         public IActionResult Index()
         {
             return View("Index");
         }
+
+        [HttpPost]
+        public IActionResult PredictFraud() //This is the method that will be called when the user clicks the "Predict Fraud" button
+            // I will receive the input data from the user and use it to make a prediction. 
+            // I will receive the input not dummy coded so I will have to do the logic to dummy code it here
+
+            // Exaple: I will receive "country" as a string, but the model expects it to be a number.
+            // I will have to write the logic to recognize if the country is "USA", "Canada", "Mexico", etc. and convert it to a number
+        {
+            //This is the input data that will be used to make the prediction
+
+            // A Tensor is a multi-dimensional array. In this case, we are creating a 1D tensor with 30 elements
+            var input = new List<float> { /* input variables*/};
+            var inputTensor = new DenseTensor<float>(input.ToArray(), new[] {1, input.Count });
+
+            var inputs = new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("input", inputTensor)
+            };
+
+            using (var result = _sessionFraud.Run(inputs)) // This is where the prediction is made
+            // The output will be true or false, depending on whether the model thinks the transaction is fraudulent
+            {
+                var output = result.First().AsTensor<bool>().ToArray();
+                if (output[0])
+                {
+                    //return RedirectToAction("FraudConfirmation");
+                }
+                else
+                {
+                    //return RedirectToAction("Confirmation");
+                }
+
+
+                return View(); //This is just a placeholder. I will change it to a redirect to the appropriate view
+            }
+        }
+                   
+        
+
 
         public IActionResult Products(int pageNum = 1, string? productCategory = null)
         {
@@ -80,6 +133,8 @@ namespace INTEX_II_413.Controllers
 
         public IActionResult Orders()
         {
+            // This is where the admin will see all the orders that have been placed
+            // Specifically they will see orders that have been flagged as fraudulent
             return View();
         }
 
