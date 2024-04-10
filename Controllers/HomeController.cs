fdc9b0775fe79a5b58fe342e197192ee2ddeccac
HammondCameron55 <110644424+HammondCameron55@users.noreply.github.com>
2024-04-10 15:31:26 -0600

    Very Messy Right now, I just wanted to save my progress

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace INTEX_II_413.Controllers
             _repo = temp;
 
             _sessionFraud = new InferenceSession("C:/Users/Hammo/source/repos/INTEX_II_413/Fraud_Identification_model.onnx");
-            
+
         }
 
 
@@ -34,19 +34,59 @@ namespace INTEX_II_413.Controllers
             return View("Index");
         }
 
-        [HttpPost]
-        public IActionResult PredictFraud() //This is the method that will be called when the user clicks the "Predict Fraud" button
-            // I will receive the input data from the user and use it to make a prediction. 
-            // I will receive the input not dummy coded so I will have to do the logic to dummy code it here
 
-            // Exaple: I will receive "country" as a string, but the model expects it to be a number.
-            // I will have to write the logic to recognize if the country is "USA", "Canada", "Mexico", etc. and convert it to a number
+        [HttpPost]
+        public IActionResult PlaceOrder(OrderViewModel orderData) //Fix this to take in the correct data
         {
-            //This is the input data that will be used to make the prediction
+            FraudPredictionViewModel fraudPredictionData = new FraudPredictionViewModel
+            {
+                // Map orderData and possibly query the database for additional data needed
+                Time = orderData.Time,
+                Amount = orderData.Amount,
+                Age = // Calculate based on customer's birthdate,
+                CountryOfTransaction = orderData.Country,
+                ShippingAddress = orderData.Address,
+                Bank = orderData.Bank,
+                TypeOfCard = orderData.CardType,
+                CountryOfResidence = // This might be the same as CountryOfTransaction or different based on logic,
+                Gender = // Get from Customer data,
+
+
+                };
+
+            bool isFraudulent = PredictFraud(orderData);
+
+            if (isFraudulent)
+            {
+                // Redirect to a fraud confirmation page or handle accordingly
+                return RedirectToAction("FraudConfirmation");
+            }
+            else
+            {
+                // Process the order normally
+                return RedirectToAction("Confirmation");
+            }
+        }
+        [HttpPost]
+        public IActionResult PredictFraud(OrderViewModel orderData)
+        {
 
             // A Tensor is a multi-dimensional array. In this case, we are creating a 1D tensor with 30 elements
-            var input = new List<float> { /* input variables*/};
-            var inputTensor = new DenseTensor<float>(input.ToArray(), new[] {1, input.Count });
+            var inputList = new List<float>();
+
+            // Add logic to fill inputList based on orderData
+
+            // Example dummy coding - adjust according to actual orderData structure
+            inputList.Add(orderData.TransactionId);
+            // Add more inputs based on the expected columns
+
+            // Considering 'entry_mode_CVC' as not present, hence 'entry_mode_PIN' and 'entry_mode_Tap' will be 0
+            // Since the type of transaction is always 'Online', set 'type_of_transaction_Online' to 1 and 'type_of_transaction_POS' to 0
+            inputList.AddRange(new float[] { 0, 0, 1, 0 }); // Adjust indices based on actual input requirements
+
+
+
+            var inputTensor = new DenseTensor<float>(inputList.ToArray(), new[] { 1, inputList.Count });
 
             var inputs = new List<NamedOnnxValue>
             {
@@ -54,24 +94,13 @@ namespace INTEX_II_413.Controllers
             };
 
             using (var result = _sessionFraud.Run(inputs)) // This is where the prediction is made
-            // The output will be true or false, depending on whether the model thinks the transaction is fraudulent
             {
                 var output = result.First().AsTensor<bool>().ToArray();
-                if (output[0])
-                {
-                    //return RedirectToAction("FraudConfirmation");
-                }
-                else
-                {
-                    //return RedirectToAction("Confirmation");
-                }
-
-
-                return View(); //This is just a placeholder. I will change it to a redirect to the appropriate view
+                return output[0]; // Return the prediction result
             }
         }
-                   
-        
+
+
 
 
         public IActionResult Products(int pageNum = 1, string? productCategory = null)
@@ -142,26 +171,26 @@ namespace INTEX_II_413.Controllers
                 // I will need to display the order number, the customer's name, the date the order was placed, and the total amount of the order
                 // I will also need to display the products that were ordered
             }
-            
+
             return View();
         }
 
-            var product = _repo.Products.Where(p => p.ProductId == id).FirstOrDefault();
+        var product = _repo.Products.Where(p => p.ProductId == id).FirstOrDefault();
 
             return View(product);
-        }
-        public IActionResult NewUser()
-        {
-            return View();
-        }
+    }
+    public IActionResult NewUser()
+    {
+        return View();
+    }
 
-        [HttpPost]
-        public IActionResult CreateAccount()
-        {
-            return View("NewUser");
-        }
+    [HttpPost]
+    public IActionResult CreateAccount()
+    {
+        return View("NewUser");
+    }
 
-        
+}
 
         //[HttpPost]
         //public IActionResult AddProduct(Product response)
@@ -170,5 +199,3 @@ namespace INTEX_II_413.Controllers
         //    _repo.SaveChanges();
         //    return View("AdminProducts");
         //}
-    }
-}
---
+++ Models/User_Based_Recs.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace INTEX_II_413.Models
+{
+    public class User_Based_Recs
+    {
+        [Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public int Recommendation1 { get; set; }
+
+        [Required]
+        public int Recommendation2 { get; set; }
+
+        [Required]
+        public int Recommendation3 { get; set; }
+
+        [Required]
+        public int Recommendation4 { get; set; }
+
+        // Navigation properties for the product and its recommendations
+        // These establish relationships to the Product model
+
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+
+        [ForeignKey("Recommendation1")]
+        public virtual Product RecommendedProduct1 { get; set; }
+
+        [ForeignKey("Recommendation2")]
+        public virtual Product RecommendedProduct2 { get; set; }
+
+        [ForeignKey("Recommendation3")]
+        public virtual Product RecommendedProduct3 { get; set; }
+
+        [ForeignKey("Recommendation4")]
+        public virtual Product RecommendedProduct4 { get; set; }
+    }
+}
