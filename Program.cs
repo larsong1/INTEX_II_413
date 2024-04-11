9ed24f4cd221a96cf2cff00ec4345666133ed3bb
jd1010756 <106291794+j1010756@users.noreply.github.com>
2024-04-11 19:36:26 -0400

    new

--- Program.cs
+++ Program.cs
@@ -93,12 +93,10 @@ namespace INTEX_II_413
                 string inlineScriptNonce2 = Guid.NewGuid().ToString("N");
 
                 // Add nonce to CSP header
-                context.Response.Headers.Add("Content-Security-Policy", $"default-src 'self'; script-src 'self' 'nonce-{cookieNonce}' 'nonce-{inlineScriptNonce1}' 'nonce-{inlineScriptNonce2}' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' https://www.thesun.co.uk https://www.lego.com https://images.brickset.com data: https://m.media-amazon.com https://www.brickeconomy.com https://localhost:7094; font-src 'self'; connect-src 'self' https://localhost:44337 http://localhost:40904 ws: wss:; frame-src 'self';");
+                context.Response.Headers.Add("Content-Security-Policy", $"default-src 'self'; script-src 'self' 'nonce-{cookieNonce}' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' https://www.thesun.co.uk https://www.lego.com https://images.brickset.com data: https://m.media-amazon.com https://www.brickeconomy.com https://localhost:7094; font-src 'self'; connect-src 'self' https://localhost:44337 http://localhost:40904 ws: wss:; frame-src 'self';");
 
                 // Pass the nonce value to your view
                 context.Items["CookieCspNonce"] = cookieNonce;
-                context.Items["InlineCspNonce1"] = inlineScriptNonce1;
-                context.Items["InlineCspNonce2"] = inlineScriptNonce2;
 
                 await next();
             });
