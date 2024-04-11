ff965218e563ce04fc67c16f4109bc65620e4770
jd1010756 <106291794+j1010756@users.noreply.github.com>
2024-04-11 19:39:38 -0400

    fix

--- Program.cs
+++ Program.cs
@@ -85,21 +85,7 @@ namespace INTEX_II_413
 
             var app = builder.Build();
 
-            app.Use(async (context, next) =>
-            {
-                // Generate a nonce
-                string cookieNonce = Guid.NewGuid().ToString("N");
-                string inlineScriptNonce1 = Guid.NewGuid().ToString("N");
-                string inlineScriptNonce2 = Guid.NewGuid().ToString("N");
 
-                // Add nonce to CSP header
-                context.Response.Headers.Add("Content-Security-Policy", $"default-src 'self'; script-src 'self' 'nonce-{cookieNonce}' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' https://www.thesun.co.uk https://www.lego.com https://images.brickset.com data: https://m.media-amazon.com https://www.brickeconomy.com https://localhost:7094; font-src 'self'; connect-src 'self' https://localhost:44337 http://localhost:40904 ws: wss:; frame-src 'self';");
-
-                // Pass the nonce value to your view
-                context.Items["CookieCspNonce"] = cookieNonce;
-
-                await next();
-            });
 
 
 
