7e10ca8e9222d87d53df7d149bd52c7280ce360c
jd1010756 <106291794+j1010756@users.noreply.github.com>
2024-04-10 21:02:23 -0600

    cookie

--- Program.cs
+++ Program.cs
@@ -85,12 +85,20 @@ namespace INTEX_II_413
 
             var app = builder.Build();
 
-            // Set CSP policy
+            // Generate a nonce value
+            var nonce = Guid.NewGuid().ToString();
+
+            // Set CSP policy including nonce
             app.Use(async (context, next) =>
             {
-                context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' https://www.thesun.co.uk https://www.lego.com https://images.brickset.com data: https://m.media-amazon.com https://www.brickeconomy.com; font-src 'self'; connect-src 'self' http://localhost:23148 https://localhost:44337 ws: wss:; frame-src 'self';");
+                context.Response.Headers.Add("Content-Security-Policy", $"default-src 'self'; script-src 'self' 'nonce-{nonce}'; style-src 'self' 'unsafe-inline'; img-src 'self' https://www.thesun.co.uk https://www.lego.com https://images.brickset.com data: https://m.media-amazon.com https://www.brickeconomy.com; font-src 'self'; connect-src 'self' http://localhost:23148 https://localhost:44337 ws: wss:; frame-src 'self';");
+                context.Response.Headers.Add("X-Content-Security-Policy", $"default-src 'self'; script-src 'self' 'nonce-{nonce}'; style-src 'self' 'unsafe-inline'; img-src 'self' https://www.thesun.co.uk https://www.lego.com https://images.brickset.com data: https://m.media-amazon.com https://www.brickeconomy.com; font-src 'self'; connect-src 'self' http://localhost:23148 https://localhost:44337 ws: wss:; frame-src 'self';");
+                context.Response.Headers.Add("X-WebKit-CSP", $"default-src 'self'; script-src 'self' 'nonce-{nonce}'; style-src 'self' 'unsafe-inline'; img-src 'self' https://www.thesun.co.uk https://www.lego.com https://images.brickset.com data: https://m.media-amazon.com https://www.brickeconomy.com; font-src 'self'; connect-src 'self' http://localhost:23148 https://localhost:44337 ws: wss:; frame-src 'self';");
+
+                // Pass the request to the next middleware
                 await next();
             });
+
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
